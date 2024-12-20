a50615da6f93eb15eda14dea75e0e0415029793c
Kumar <kumaravel04042015@gmail.com>
2024-12-20 22:43:37 +0530

    Day 1 22:43

---
+++ Week 1/13.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    pair<long long, long long> getMinMax(vector<long long int> arr) {
+        long long min,max;
+        min=max=arr[0];
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            else if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        pair<long long,long long> g={min,max};
+        return g;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
